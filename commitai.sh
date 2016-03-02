e5a2ada4325a3e26e249aa7a80d9fc930af1b7b4
syednasar <syed.nasar@gatech.edu>
2016-03-02 05:19:34 -0600

    Adding git commit script that takes params

--- commitai.sh
+++ commitai.sh
@@ -1,5 +1,18 @@
+#!/bin/sh
+
+commitDesc="Added via script."
+
+if [ $# -ne 0 ]
+  then
+    echo $1
+    commitDesc=$1
+fi
+
+
 git add .
 
-git commit -m "added via script"
+git status
+
+git commit -m "$commitDesc"
 
 git push
